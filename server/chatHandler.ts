c097c1a6da67d061e0ce38dc6ffaf7f271a77175
Millie Cheung <cheungmillie9@gmail.com>
2022-05-04 15:39:49 +0200

    showing number of users and list of users in console.log

--- server/chatHandler.ts
+++ server/chatHandler.ts
@@ -2,13 +2,25 @@ import { Server, Socket } from "socket.io";
 import { getRooms } from "./roomStore";
 
 export default (io: Server, socket: Socket) => {
-  socket.on("join", (room) => {
+  socket.on("join", async (room: string) => {
     const shouldBroadcastRooms: boolean = !getRooms(io).includes(room); //Om det ya rummet inte finns med i listan getRooms, då blir det true och skapar ett nytt rum
 
     socket.join(room);
     if (shouldBroadcastRooms) {
       io.emit("roomList", getRooms(io));
     }
+
+    // show number of users in the room (in console log for now)
+    const numClients = io.sockets.adapter.rooms.get(room)?.size;
+    console.log(numClients);
+
+    // show a list of the clients in the room (in console log for now)
+    let roomUsers: string[] = [];
+
+    (await io.in(room).fetchSockets()).map((item) =>
+      roomUsers.push(item.data.nickname)
+    );
+    console.log(roomUsers);
 
     socket.emit("joined", room);
   });
--- server/roomStore.ts
+++ server/roomStore.ts
@@ -1,7 +1,7 @@
 import { Server } from "socket.io";
 
 export function getRooms(io: Server) {
-  const rooms = [];
+  const rooms: string[] = [];
   for (let [id, socket] of io.sockets.adapter.rooms) {
     if (!socket.has(id)) {
       rooms.push(id);
@@ -9,3 +9,19 @@ export function getRooms(io: Server) {
   }
   return rooms;
 }
+
+// doesnt work for now......
+/*
+export const getListOfClients = (io: Server) => {
+  // show a list of the clients in the room (in console log for now)
+  let roomUsers: string[] = [];
+  getRooms(io).map(async (room) =>
+    (await io.in(room).fetchSockets()).map((sockets) => {
+      roomUsers.push(sockets.data.nickname);
+      console.log(sockets.data.nickname);
+    })
+  );
+  console.log(roomUsers);
+  return roomUsers;
+};
+*/
