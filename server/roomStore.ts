a4c3d3ea54cd2bec7d46eef50a38b78bec727017
William Saar <williamsaar@outlook.com>
2022-05-07 21:39:34 +0200

    changed the function that gets all the rooms

--- server/roomStore.ts
+++ server/roomStore.ts
@@ -1,11 +1,19 @@
 import { Server } from "socket.io";
+import { ChatRoom, ServerSocketData } from "../types";
 
-export function getRooms(io: Server) {
-  const rooms: string[] = [];
-  for (let [id, socket] of io.sockets.adapter.rooms) {
-    if (!socket.has(id)) {
-      rooms.push(id);
-    }
+export function getRooms(io: Server): ChatRoom[] {
+  const rooms: ChatRoom[] = [];
+  for (const [id, socketIds] of io.sockets.adapter.rooms) {
+    if (socketIds.has(id)) continue; // Skip self
+    const sockets = Array.from(socketIds).map((id) =>
+      io.sockets.sockets.get(id)
+    );
+    rooms.push({
+      name: id,
+      sockets: sockets
+        .filter((s) => s?.data)
+        .map((s) => s?.data) as ServerSocketData[],
+    });
   }
   return rooms;
 }
