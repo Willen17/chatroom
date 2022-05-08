283e69edfb439eff88b1fc5314940e22bc7fadf6
Millie Cheung <cheungmillie9@gmail.com>
2022-05-08 19:00:45 +0200

    shows clients by clicking the number next to the room name

--- client/src/ListOfRooms.tsx
+++ client/src/ListOfRooms.tsx
@@ -1,10 +1,13 @@
-import { Box, Button, Typography } from "@mui/material";
-import { useEffect } from "react";
+import { Box, Button, Grow, Typography } from "@mui/material";
+import React, { useState } from "react";
 import { useSocket } from "./SocketContext";
 
 const ListOfRooms = () => {
   const { rooms, currentRoom, socket, setCurrentRoom, clientList } =
     useSocket();
+  const [checked, setChecked] = useState(false);
+  // const [target, setTarget] = useState("");
+  const [expandedId, setExpandedId] = useState(-1);
 
   const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
@@ -16,6 +19,23 @@ const ListOfRooms = () => {
       socket!.emit("join", theRoom);
       console.log("från under room");
     }
+  };
+
+  const handleExpandClick = (i: number) => {
+    console.log("index: ", i);
+    setExpandedId(expandedId === i ? -1 : i);
+    console.log("expandedID: ", expandedId);
+  };
+
+  // const clickedRoom = (e: React.MouseEvent<HTMLButtonElement>) => {
+  //   const target1 = e.currentTarget.parentElement?.firstChild?.textContent;
+  //   setTarget(target1 || "");
+  //   console.log("target:", target);
+  //   showClients(e);
+  // };
+
+  const showClients = (e: React.MouseEvent<HTMLButtonElement>) => {
+    checked ? setChecked(false) : setChecked(true);
   };
 
   // useEffect(() => {
@@ -41,28 +61,66 @@ const ListOfRooms = () => {
             }}
           >
             <Box>
-              <Button
-                variant="text"
-                size="small"
+              <Box
                 sx={{
-                  fontFamily: "League Spartan",
-                  letterSpacing: "none",
-                  textTransform: "none",
-                  fontSize: "1rem",
-                  color: "#F2CC8F",
-                  boxShadow: "none",
+                  display: "flex",
+                  flexDirection: "row",
+                  placeItems: "center",
                 }}
-                onClick={handleSubmit}
               >
-                {room.name}
-              </Button>
-              <div>
+                <Button
+                  variant="text"
+                  size="small"
+                  sx={{
+                    fontFamily: "League Spartan",
+                    letterSpacing: "none",
+                    textTransform: "none",
+                    fontSize: "1rem",
+                    color: "#F2CC8F",
+                    boxShadow: "none",
+                    minWidth: "auto",
+                  }}
+                  onClick={handleSubmit}
+                >
+                  {room.name}
+                </Button>
+                <Typography
+                  key={index}
+                  variant="overline"
+                  color="#8184A7"
+                  sx={{
+                    fontFamily: "League Spartan",
+                    letterSpacing: "none",
+                    cursor: "pointer",
+                  }}
+                  onClick={showClients}
+                >
+                  ({room.sockets.length})
+                </Typography>
+              </Box>
+              <Box sx={{ height: checked ? "fit-content" : 0 }}>
                 {room.sockets.map((user, index) => (
-                  <Typography key={index} variant="body2">
-                    {user.nickname}
-                  </Typography>
+                  <Grow
+                    key={index}
+                    // orientation="horizontal"
+                    in={checked}
+                    {...(checked ? { timeout: 500 } : { timeout: 0 })}
+                  >
+                    <Typography
+                      color="#f4f1de"
+                      variant="body2"
+                      sx={{
+                        fontFamily: "League Spartan",
+                        letterSpacing: "none",
+                        pl: "1rem",
+                        transition: 0,
+                      }}
+                    >
+                      {user.nickname}
+                    </Typography>
+                  </Grow>
                 ))}
-              </div>
+              </Box>
             </Box>
           </li>
         ))}
