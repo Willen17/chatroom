a1d51e919c01af90ef0608986aac9b986fbcb10b
William Saar <williamsaar@outlook.com>
2022-05-10 20:15:24 +0200

    sessionID saves to localstorage, now if user refreshes, he stayson same user

--- server/server.ts
+++ server/server.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from "crypto";
 import { Server, Socket } from "socket.io";
 import {
   ClientToServerEvents,
@@ -9,6 +10,7 @@ import {
 import registerChatHandler from "./chatHandler";
 import { getIDFromName } from "./directMessages";
 import { getRooms, getUsers } from "./roomStore";
+import { findSession, saveSession } from "./sessionStore";
 
 const io = new Server<
   ClientToServerEvents,
@@ -18,10 +20,26 @@ const io = new Server<
 >({ path: "/socket" });
 
 io.use((socket: Socket, next) => {
+  const sessionID = socket.handshake.auth.sessionID;
+  if (sessionID) {
+    //find existing session
+    const session = findSession(sessionID);
+    if (session) {
+      socket.data.sessionID = sessionID;
+      socket.data.userID = session.userID;
+      socket.data.nickname = session.nickname;
+      return next();
+    }
+  }
+
   const nickname: string = socket.handshake.auth.nickname;
   if (!nickname || nickname.length < 3) {
     return next(new Error("Invalid nickname"));
   }
+
+  //create new session
+  socket.data.sessionID = randomUUID();
+  socket.data.userID = randomUUID();
   socket.data.nickname = nickname;
   next();
 });
@@ -29,6 +47,17 @@ io.use((socket: Socket, next) => {
 io.on("connection", (socket) => {
   console.log("a user connected");
 
+  saveSession(socket.data.sessionID!, {
+    userID: socket.data.userID,
+    nickname: socket.data.nickname,
+    connected: true,
+  });
+
+  socket.emit("initSession", {
+    sessionID: socket.data.sessionID!,
+    userID: socket.data.userID!,
+    nickname: socket.data.nickname!,
+  });
   if (socket.data.nickname) {
     socket.emit("connected", {
       userID: socket.id,
