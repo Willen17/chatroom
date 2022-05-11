93d459be214418d110f677231c0c616b8d8e4a0a
William Saar <williamsaar@outlook.com>
2022-05-11 11:14:43 +0200

    added fucntion to set online status of users, and also made it so their can be no duplicates

--- server/roomStore.ts
+++ server/roomStore.ts
@@ -18,14 +18,32 @@ export function getRooms(io: Server): ChatRoom[] {
   return rooms;
 }
 
-export function getUsers(io: Server): User[] {
-  const users: User[] = [];
+let users: User[] = [];
 
+// This function sets isConnected to false
+export function setDisconnected(io: Server, userID: string) {
+  let index = users.findIndex((user) => user.userID === userID);
+  users[index].isConnected = false;
+}
+
+export function getUsers(io: Server): User[] {
   for (let [id, socket] of io.sockets.sockets) {
-    users.push({
-      userID: id,
-      username: socket.data.nickname as string,
-    });
+    //If the user.id already exists in the array, we will set the userID to same as before
+    if (users.some((user) => user.userID === socket.data.userID)) {
+      let index = users.findIndex((user) => user.userID === socket.data.userID);
+      users[index].userID = socket.data.userID;
+      //If isConnected is set to false, we keep it as false.
+    } else if (users.some((user) => user.isConnected === false)) {
+      let index = users.findIndex((user) => user.isConnected === false);
+      users[index].isConnected = false;
+      // If non of the above is true, then it means it is a new user, and then we push it to the array
+    } else {
+      users.push({
+        userID: socket.data.userID,
+        username: socket.data.nickname,
+        isConnected: true,
+      });
+    }
   }
   return users;
 }
