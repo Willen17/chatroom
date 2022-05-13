9c7b5f2a90e4a738da4de5c34e1778c3bbc0de20
Millie Cheung <cheungmillie9@gmail.com>
2022-05-13 10:00:32 +0200

    removed console.log, unused code and added comments

--- server/roomMessageStore.ts
+++ server/roomMessageStore.ts
@@ -2,6 +2,7 @@ import { MessageType } from "../types";
 
 let roomMessageStore = new Map<string, MessageType[]>();
 
+// save messages in the room message store
 export const addMessageToRoomMessageStore = (
   roomName: string,
   message: string,
@@ -11,10 +12,10 @@ export const addMessageToRoomMessageStore = (
     roomMessageStore.set(roomName, []);
   }
   const chatHistory = roomMessageStore.get(roomName)!;
-
   chatHistory.push({ message, from });
 };
 
+// retrieve message history of a specific room from the room message store
 export const getRoomMessageHistory = (roomName: string) => {
   return roomMessageStore.get(roomName);
 };
